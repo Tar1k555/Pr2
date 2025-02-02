60fe4746045a3158793e0a85e55da67f647e2609
Tar1k555 <lahnuk33@gmail.com>
2025-02-02 15:53:15 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-    // Ховаємо всі секції
-    document.querySelectorAll('.content-section').forEach(section => {
-        section.style.display = 'none';
-    });
-
-    // Відображаємо вибрану секцію
-    document.getElementById(sectionId).style.display = 'block';
-}
